6633986c6281510c9367a69cbdd42a066804d09f
koskh <koskh@yandex.ru>
2024-03-31 19:44:48 +0200

    #main ~redux actions for mvc

--- src/state/gameController.ts
+++ src/state/gameController.ts
@@ -1,13 +1,50 @@
-export default function counterReducer(
-  state = { value: 0 },
-  action: { type: unknown },
-) {
-  switch (action.type) {
-    case 'counter/incremented':
-      return { value: state.value + 1 };
-    case 'counter/decremented':
-      return { value: state.value - 1 };
-    default:
-      return state;
-  }
-}
+import { createSlice } from '@reduxjs/toolkit';
+import initialState from './initialState';
+
+export const gameController = createSlice({
+  name: 'testName',
+  initialState,
+  reducers: {
+    setInmunity: (state, action) => {
+      console.log('setInmunity');
+      state.inmunity = action.payload;
+    },
+
+    moveTo: (state, action) => {
+      // mutate the state all you want with immer
+      state.moveTo = action.payload;
+
+      // animation
+      let animation = 'turn';
+      if (state.moveTo.left) {
+        animation = 'left';
+      } else if (state.moveTo.right) {
+        animation = 'right';
+      }
+
+      // velocity
+      // state.velocity = {
+      //   x: state.config.velocity * ((state.moveTo.left * -1) + (state.moveTo.right * 1)),
+      //   y: state.config.velocity * state.moveTo.up * -2,
+      //   animation
+      // };
+
+      // Reduce Inmunity
+      state.inmunity--;
+    },
+
+    collision: (state, action) => {
+      if (action.payload === 'bomb') {
+        // Game Over
+        state.gameOver = state.inmunity < 1;
+      } else {
+        // Change score
+        // state.score = state.score + state.config.score[action.payload]; // mutate the state all you want with immer
+      }
+    },
+
+    setInfo: (state, action) => {
+      state.info = action.payload; // mutate the state all you want with immer
+    },
+  },
+});
--- src/state/gameModel.ts
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'redux';
-import gameController from './gameController';
-
-const store = createStore(gameController);
-
-// store.subscribe(() => console.log('store.getState', store.getState()));
-
-export { store as gameStore, gameController };
