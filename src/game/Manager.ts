ed21ee8a17fc3b36239718493360d4978f211cb6
koskh <koskh@yandex.ru>
2024-04-02 14:56:00 +0200

    #mvc, ~makeup and render game board

--- src/game/Manager.ts
+++ src/game/Manager.ts
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/no-this-alias */
 import Board from './Board';
+import Phaser from 'phaser';
 
 export let gameManager: GameManager;
 export default class GameManager {
+  // scene: Phaser.Scene
   board: Board;
 
   constructor() {
